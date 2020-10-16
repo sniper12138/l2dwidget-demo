949ee2d954dd7ccf96bed0574147e27cb15621fd
张璐 <1774988296@qq.com>
2020-10-16 18:29:33 +0800

    update: 添加刷新按钮

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img class="img" alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,6 +17,15 @@ export default {
 </script>
 
 <style lang="less">
+html,body{
+  width: 100%;
+  height: 100%;
+  overflow-y: hidden;
+  .img{
+    -webkit-filter: blur(10px);
+    filter: blur(10px);
+  }
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,80 +1,89 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <div class="text">
+      <h1>{{ msg }}</h1>
+      <p>
+        For a guide and recipes on how to configure / customize this project,<br />
+        check out the
+        <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+          >vue-cli documentation</a
+        >.
+      </p>
+      <h3>Installed CLI Plugins</h3>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+            target="_blank"
+            rel="noopener"
+            >babel</a
+          >
+        </li>
+      </ul>
+      <h3>Essential Links</h3>
+      <ul>
+        <li>
+          <a href="https://vuejs.org" target="_blank" rel="noopener"
+            >Core Docs</a
+          >
+        </li>
+        <li>
+          <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
+            >Forum</a
+          >
+        </li>
+        <li>
+          <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
+            >Community Chat</a
+          >
+        </li>
+        <li>
+          <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
+            >Twitter</a
+          >
+        </li>
+        <li>
+          <a href="https://news.vuejs.org" target="_blank" rel="noopener"
+            >News</a
+          >
+        </li>
+      </ul>
+      <h3>Ecosystem</h3>
+      <ul>
+        <li>
+          <a href="https://router.vuejs.org" target="_blank" rel="noopener"
+            >vue-router</a
+          >
+        </li>
+        <li>
+          <a href="https://vuex.vuejs.org" target="_blank" rel="noopener"
+            >vuex</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/vuejs/vue-devtools#vue-devtools"
+            target="_blank"
+            rel="noopener"
+            >vue-devtools</a
+          >
+        </li>
+        <li>
+          <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
+            >vue-loader</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/vuejs/awesome-vue"
+            target="_blank"
+            rel="noopener"
+            >awesome-vue</a
+          >
+        </li>
+      </ul>
+    </div>
+    <button class="reset" @click="initModel">换个人物</button>
   </div>
 </template>
 
@@ -85,48 +94,52 @@ export default {
   props: {
     msg: String,
   },
+  data() {
+    return {
+      L2Ddatas: [
+        "shizuku",
+        "tororo",
+        "unitychan",
+        "wanko",
+        "z16",
+        "miku",
+        "ni-j",
+        "nico",
+        "nipsilon",
+        "nito",
+        "haruto",
+        "chitose",
+        "hibiki",
+        "hijiki",
+        "izumi",
+        "koharu",
+      ],
+      modelUrl: "",
+      opt: {
+        model: {
+          jsonPath: "",
+        },
+        display: {
+          position: "right", //定位
+          width: 400, //宽度
+          height: 1400, //高度
+          hOffset: document.body.clientWidth / 2 - 200, //左右
+          vOffset: document.body.clientHeight / 2 - 700, //上下
+        },
+        mobile: {
+          show: false,
+        },
+        log: false,
+        react: {
+          // 设置透明度
+          opacityDefault: 1,
+          opacityOnHover: 0.5,
+        },
+      },
+    };
+  },
   mounted() {
-    let L2Ddatas = [
-      "shizuku",
-      "tororo",
-      "unitychan",
-      "wanko",
-      "z16",
-      "miku",
-      "ni-j",
-      "nico",
-      "nipsilon",
-      "nito",
-      "haruto",
-      "chitose",
-      "hibiki",
-      "hijiki",
-      "izumi",
-      "koharu",
-    ];
-    let model = L2Ddatas[this.random(0, L2Ddatas.length - 1)];
-    L2Dwidget.init({
-      model: {
-        jsonPath:
-          `https://cdn.jsdelivr.net/gh/wangsrGit119/wangsr-image-bucket/L2Dwidget/live2d-widget-model-${model}/assets/${model}.model.json`,
-      },
-      display: {
-        position: "right", //定位
-        width: 200, //宽度
-        height: 500, //高度
-        hOffset: 50, //左右
-        vOffset: 50, //上下
-      },
-      mobile: {
-        show: false,
-      },
-      log: false,
-      react: {
-        // 设置透明度
-        opacityDefault: 1,
-        opacityOnHover: 0.5,
-      },
-    });
+    this.initModel()
   },
   methods: {
     random(min, max) {
@@ -136,12 +149,28 @@ export default {
       }
       return min + Math.floor(Math.random() * (max - min + 1));
     },
+    initModel() {
+      let model = this.L2Ddatas[this.random(0, this.L2Ddatas.length - 1)];
+      let modelUrl = `https://cdn.jsdelivr.net/gh/wangsrGit119/wangsr-image-bucket/L2Dwidget/live2d-widget-model-${model}/assets/${model}.model.json`;
+      this.opt.model.jsonPath = modelUrl;
+      L2Dwidget.init(this.opt);
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
+.hello {
+  .text {
+    pointer-events: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    user-select: none;
+    -webkit-filter: blur(10px);
+    filter: blur(10px);
+  }
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -155,5 +184,40 @@ li {
 }
 a {
   color: #42b983;
+}
+.reset {
+  display: inline-block;
+  margin-bottom: 0;
+  font-weight: 400;
+  text-align: center;
+  vertical-align: middle;
+  -ms-touch-action: manipulation;
+  touch-action: manipulation;
+  cursor: pointer;
+  background-image: none;
+  border: 1px solid transparent;
+  white-space: nowrap;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  height: 32px;
+  padding: 0 15px;
+  font-size: 14px;
+  border-radius: 4px;
+  transition: color 0.2s linear, background-color 0.2s linear,
+    border 0.2s linear, box-shadow 0.2s linear;
+  color: #fff;
+  background-color: #2d8cf0;
+  border-color: #2d8cf0;
+  outline: none;
+  line-height: 1.5;
+  right: 100px;
+  bottom: 100px;
+  position: absolute;
+  &:hover {
+    background-color: #57a3f3;
+    border-color: #57a3f3;
+  }
 }
 </style>
